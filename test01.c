b76e696b48b1982f34bd9f61248307af3de3443b
coderMA <87483828+wuhucoder@users.noreply.github.com>
2022-11-22 21:02:32 +0800

    Add files via upload

---
+++ test01.c
@@ -0,0 +1,170 @@
+/* Sample code for /dev/kvm API
+ *
+ * Copyright (c) 2015 Intel Corporation
+ * Author: Josh Triplett <josh@joshtriplett.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+//这个教学代码修改自 https://lwn.net/Articles/658512/
+#include <err.h>
+#include <fcntl.h>
+#include <linux/kvm.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(void)
+{
+    int kvm, vmfd, vcpufd, ret;
+    const uint8_t code[] = {
+        0xba, 0xf8, 0x03,
+        0x00, 0xd8,   
+        0x04, '0',    
+        0xee,         
+        0xb0, '\n',    
+        0xee,         
+        0xb0, 'H',       
+        0xee,             
+        0xb0, 'e',       
+        0xee,             
+        0xb0, 'l',       
+        0xee,             
+        0xb0, 'l',       
+        0xee,             
+        0xb0, 'o',       
+        0xee,             
+        0xb0, ' ',       
+        0xee,             
+        0xb0, 'w',       
+        0xee,             
+        0xb0, 'o',       
+        0xee,             
+        0xb0, 'r',       
+        0xee,             
+        0xb0, 'l',       
+        0xee,             
+        0xb0, 'd',       
+        0xee,             
+        0xb0, '\n',       
+        0xee,             
+        0xf4,            
+    };// 定义的机器码
+    uint8_t *mem;
+    struct kvm_sregs sregs;
+    size_t mmap_size;
+    struct kvm_run *run;
+
+    kvm = open("/dev/kvm", O_RDWR | O_CLOEXEC);// 打开KVM模块设备文件
+    if (kvm == -1)
+        err(1, "/dev/kvm");
+
+    // 确保 API 版本可用
+    ret = ioctl(kvm, KVM_GET_API_VERSION, NULL);
+    if (ret == -1)
+        err(1, "KVM_GET_API_VERSION");
+    if (ret != 12)
+        errx(1, "KVM_GET_API_VERSION %d, expected 12", ret);
+
+    // 创建虚拟机获得虚拟机文件描述符
+    vmfd = ioctl(kvm, KVM_CREATE_VM, (unsigned long)0);
+    if (vmfd == -1)
+        err(1, "KVM_CREATE_VM");
+
+    // 分配一个页存放 helloworld 二进制代码
+    mem = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if (!mem)
+        err(1, "allocating guest memory");
+    memcpy(mem, code, sizeof(code)); 
+
+    // 内存页映射到GPA 0x1000 处
+    struct kvm_userspace_memory_region region = {
+        .slot = 0,
+        .guest_phys_addr = 0x1000,
+        .memory_size = 0x1000,
+        .userspace_addr = (uint64_t)mem,
+    };
+    ret = ioctl(vmfd, KVM_SET_USER_MEMORY_REGION, &region);
+    if (ret == -1)
+        err(1, "KVM_SET_USER_MEMORY_REGION");
+    //创建 vcpu 获得描述符
+    vcpufd = ioctl(vmfd, KVM_CREATE_VCPU, (unsigned long)0);
+    if (vcpufd == -1)
+        err(1, "KVM_CREATE_VCPU");
+
+    // 获取共享内存大小并映射 kvm run
+    ret = ioctl(kvm, KVM_GET_VCPU_MMAP_SIZE, NULL);
+    if (ret == -1)
+        err(1, "KVM_GET_VCPU_MMAP_SIZE");
+    mmap_size = ret;
+    if (mmap_size < sizeof(*run))
+        errx(1, "KVM_GET_VCPU_MMAP_SIZE unexpectedly small");
+    run = mmap(NULL, mmap_size, PROT_READ | PROT_WRITE, MAP_SHARED, vcpufd, 0);
+    if (!run)
+        err(1, "mmap vcpu");
+
+    // 初始化 CS 寄存器 vcpu 从 0x1000 运行
+    ret = ioctl(vcpufd, KVM_GET_SREGS, &sregs);
+    if (ret == -1)
+        err(1, "KVM_GET_SREGS");
+    sregs.cs.base = 0;
+    sregs.cs.selector = 0;
+    ret = ioctl(vcpufd, KVM_SET_SREGS, &sregs);
+    if (ret == -1)
+        err(1, "KVM_SET_SREGS");
+
+    // 初始化寄存器
+    struct kvm_regs regs = {
+        .rip = 0x1000,
+        .rax = 2,
+        .rbx = 2,
+        .rflags = 0x2,
+    };
+    ret = ioctl(vcpufd, KVM_SET_REGS, &regs);
+    if (ret == -1)
+        err(1, "KVM_SET_REGS");
+
+    while (1) {
+        ret = ioctl(vcpufd, KVM_RUN, NULL);// 运行 vCPU
+        if (ret == -1)
+            err(1, "KVM_RUN");// 运行出错
+        switch (run->exit_reason) {
+        case KVM_EXIT_HLT:
+            puts("KVM_EXIT_HLT");// 退出
+            return 0;
+        case KVM_EXIT_IO:
+            if (run->io.direction == KVM_EXIT_IO_OUT && run->io.size == 1 && run->io.port == 0x3f8 && run->io.count == 1)
+                putchar(*(((char *)run) + run->io.data_offset)); //输出 helloworld
+            else
+                errx(1, "unhandled KVM_EXIT_IO");
+            break;
+        case KVM_EXIT_FAIL_ENTRY:
+            errx(1, "KVM_EXIT_FAIL_ENTRY: hardware_entry_failure_reason = 0x%llx",
+                 (unsigned long long)run->fail_entry.hardware_entry_failure_reason);
+        case KVM_EXIT_INTERNAL_ERROR:
+            errx(1, "KVM_EXIT_INTERNAL_ERROR: suberror = 0x%x", run->internal.suberror);
+        default:
+            errx(1, "exit_reason = 0x%x", run->exit_reason);
+        }
+    }
+}
